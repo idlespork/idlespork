982a61e5f8e38d3c718d9bca523379110f340734
Lior Goldberg <goldberg.lior@gmail.com>
2016-04-29 12:57:49 +0300

    Changed setup script

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='idlespork',
       version='0.1',
       description='Improved IDLE',
       packages=['idlesporklib'],
-      package_data={'idlesporklib' : ["Icons/*", "*.def", "*.txt", "licenses/*"]}
+      package_data={'idlesporklib' :
+          ["Icons/*", "*.def", "*.txt", "licenses/*", "idlespork.pyw",
+           "idlespork.bat"]},
+      scripts=['bin/idlespork'],
      )
