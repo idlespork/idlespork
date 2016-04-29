b433de65e49e548d1fc161c0496fcbd4bd45d0bd
Lior Goldberg <goldberg.lior@gmail.com>
2016-04-29 17:59:05 +0300

    pypi modifications

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@
 from setuptools import setup
 
 setup(name='idlespork',
-      version='0.1',
+      version='0.0.1',
       description='Improved IDLE',
       packages=['idlesporklib'],
       package_data={'idlesporklib' :
           ["Icons/*", "*.def", "*.txt", "licenses/*", "idlespork.pyw",
            "idlespork.bat"]},
       scripts=['bin/idlespork'],
+      url="https://github.com/idlespork/idlespork",
+      download_url="https://github.com/idlespork/idlespork/tarball/0.0.1",
+      keywords=["IDLE", "background", "jobs"],
      )
