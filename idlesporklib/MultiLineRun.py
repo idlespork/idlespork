a5e9c0f7911e6b0a578bf3d66c3e283e3a88ec82
Lior Goldberg <goldberg.lior@gmail.com>
2016-06-25 13:54:08 +0300

    Changed MultiLineRun to work more smoothly with idlespork

--- idlesporklib/MultiLineRun.py
+++ idlesporklib/MultiLineRun.py
@@ -7,8 +7,6 @@
 ##    License: See LICENSE.txt
 ##    """
 
-
-
 ##
 ##  This extension allows for pasting of multiple lines of code into the shell
 ##  for execution. This addresses http://bugs.python.org/issue3559
@@ -29,6 +27,8 @@ import time
 import re
 import sys
 import traceback
+
+INDENT_CHARS = set(" \t")
 
 class MultiLineDelegator(Delegator):
     def __init__(self, callback):
@@ -54,8 +54,7 @@ class MultiLineDelegator(Delegator):
         self.delegate.delete(index1, index2)
 
 
-class MultiLineRun:
-
+class MultiLineRun(object):
     # eol code from IOBinding.py
     eol = r"(\r\n)|\n|\r"  # \r\n (Windows), \n (UNIX), or \r (Mac)
     eol_re = re.compile(eol)
@@ -73,8 +72,6 @@ class MultiLineRun:
         if wsys == 'x11':
             self.text.bind('<Button-2>', self.paste, '+')  # For X11 middle click
 
-        self.playback_list = []
-
     def paste(self, event=None):
         self.mld.paste = True
 
@@ -88,85 +85,38 @@ class MultiLineRun:
     def play(self, chars):
         chars = self.eol_re.sub(r"\n", chars)
 
-        L = [] # list of entries to play into the shell
-        index = 0
-        while True:
-            next_index = chars.find('\n', index)
-            if next_index > -1:
-                line = chars[index:next_index]
-                L.append((line, True))
-            else:
-                line = chars[index:]
-                L.append((line, False))
-                break
-            index = next_index + 1
+        lines = chars.splitlines()
+        lines = self.dedent(lines)
 
-        L = self.dedent(L)
-        self.playback_list = L
-        self._pre_playback()
-        self.do_playback()
+        self.text.insert("insert", "\n".join(lines))
 
-    def dedent(self, L):
+    def dedent(self, lines):
         """
         remove maximal amount of indents/spaces shared by all lines
         to enable pasting an equally indented region
-        Also remove empty lines, because they cause execution half-way
         """
         # exclude commented-out lines, empty lines for calculation of greatest common indent to strip
-        Lcode = [line for line,ret in L if line.rstrip() and not line.startswith('#')]
 
-        # calculate min number of tabs
-        dedent_tab =0
-        while all(map(lambda line: line.startswith('\t'),Lcode)):
-            Lcode = [x[1:] for x in Lcode]
-            dedent_tab += 1
+        indentation = self.find_max_indent(lines)
+        indentation_len = len(indentation)
+        lines = [line[indentation_len:] if line.startswith(indentation) else line
+                 for line in lines]
+        return lines
 
-        if not dedent_tab:
-            dedent_space = 0
-            while all(map(lambda line: line.startswith(' '), Lcode)):
-                Lcode = [x[1:] for x in Lcode]
-                dedent_space += 1
-            depth = dedent_space
-            src = '\n'.join([line for line,ret in L if line.rstrip()])
-            src = re.sub(r"(?<![^\n]) {%i,%i}" % (depth, depth), "", src)
-            L = zip(src.split('\n'),[ret for line,ret in L if line.rstrip()])
-        else:
-            depth = dedent_tab
-            src = '\n'.join([line for line,ret in L if line.rstrip()])
-            src = re.sub(r"(?<![^\n])\t{%i,%i}" % (depth, depth), "", src)
-            L = zip(src.split('\n'),[ret for line,ret in L if line.rstrip()])
-
-        return L
-
-    def _pre_playback(self):
-        # newline_and_indent can be problematic - replace it momentarily
-        self.n = self.editwin.newline_and_indent_event
-        def simple_return(text = self.text):
-            text.insert('insert', '\n')
-        self.editwin.newline_and_indent_event = lambda *args, **kwargs: simple_return()
-
-    def _post_playback(self):
-        self.editwin.newline_and_indent_event = self.n
-
-    def do_playback(self):
-        self.mld.paste = False
-        e = self.editwin
-        t = self.text
-
-        if not self.playback_list or e.canceled:
-            self._post_playback()
-            return
-
-        if not e.executing:# or e.reading:
-            line, enter = self.playback_list.pop(0)
-            t.mark_gravity('iomark', 'left')
-            t.insert('insert', line)
-            if e.color:
-                e.color.recolorize()
-            if enter:
-                t.event_generate('<Return>')
-                t.see('insert')
-
-        t.after(10, self.do_playback)
-
-
+    def find_max_indent(self, lines):
+        """
+        Return the common indentation shared by all lines,
+        not including empty and comment lines
+        """
+        Lcode = [line for line in lines
+                 if line.rstrip() and not line.lstrip().startswith('#')]
+        indentation = []
+        idx = 0
+        while True:
+            cur_char = set(line[idx] for line in Lcode)
+            if len(cur_char) != 1: break
+            indent_char = list(cur_char)[0]
+            if indent_char not in INDENT_CHARS: break
+            indentation.append(indent_char)
+            idx += 1
+        return "".join(indentation)
