08bca1cc1ccfedfc2499b5c0f5413a21cb3f30dc
Dror Speiser <dror@bugatone.com>
2017-08-09 14:47:53 +0300

    working on better history

--- idlesporklib/IdleHistory.py
+++ idlesporklib/IdleHistory.py
@@ -1,65 +1,106 @@
-"Implement Idle Shell history mechanism with History class"
+"""Implement Idle Shell history mechanism with History class"""
 
 import os
 
 from idlesporklib.configHandler import idleConf
 from idlesporklib.IdlePrehistory import Prehistory
 
-class History:
-    ''' Implement Idle Shell history mechanism.
+
+class SuperHistory(object):
+    def __init__(self, history):
+        self.history = history
+
+    def __getitem__(self, item):
+        pass
+
+
+class History(object):
+    """Implement Idle Shell history mechanism.
 
     store - Store source statement (called from PyShell.resetoutput).
     fetch - Fetch stored statement matching prefix already entered.
     history_next - Bound to <<history-next>> event (default Alt-N).
     history_prev - Bound to <<history-prev>> event (default Alt-P).
-    '''
+    """
     def __init__(self, text):
-        '''Initialize data attributes and bind event methods.
+        """Initialize data attributes and bind event methods.
 
         .text - Idle wrapper of tk Text widget, with .bell().
         .history - source statements, possibly with multiple lines.
         .prefix - source already entered at prompt; filters history list.
         .pointer - index into history.
         .cyclic - wrap around history list (or not).
-        '''
+        """
         if 'SPORKPATH' not in os.environ.keys():
             self.ph = Prehistory(os.path.expanduser('~'))
         else:
             self.ph = Prehistory(os.environ['SPORKPATH'])
         self.text = text
         self.history = self.ph.get()
+        self.super_history = list(enumerate(self.history))
         self.prefix = None
         self.pointer = None
+        self.suggested = []
         self.cyclic = idleConf.GetOption("main", "History", "cyclic", 1, "bool")
         text.bind("<<history-previous>>", self.history_prev)
         text.bind("<<history-next>>", self.history_next)
 
+    def check_changed(self):
+        # If pointer and prefix are set, check if they are still "true", i.e. user didn't change line since
+        # last fetch.
+        if self.pointer is not None and self.prefix is not None:
+            if self.text.compare("insert", "!=", "end-1c") or \
+                    (self.suggested and self.text.get("iomark", "end-1c") != self.suggested[-1][0]):
+                # Things have changed - reset pointer and prefix.
+                self.pointer = self.prefix = None
+                self.suggested[:] = []
+                self.text.mark_set("insert", "end-1c")  # != after cursor move
+
     def history_next(self, event):
-        "Fetch later statement; start with ealiest if cyclic."
-        self.fetch(reverse=False)
+        """Fetch later statement; start with ealiest if cyclic."""
+        # self.fetch(reverse=False)
+
+        self.check_changed()
+
+        if len(self.suggested):
+            self.suggested.pop()
+
+            if len(self.suggested):
+                item, pointer = self.suggested[-1]
+                self.text.delete("iomark", "end-1c")
+                self.text.insert("iomark", item)
+                self.histwin.goto(self.super_history[pointer][0])
+            else:
+                if self.text.get("iomark", "end-1c") != self.prefix:
+                    self.text.delete("iomark", "end-1c")
+                    self.text.insert("iomark", self.prefix)
+                self.pointer = self.prefix = None
+                self.text.see("insert")
+                self.text.tag_remove("sel", "1.0", "end")
+
         return "break"
 
     def history_prev(self, event):
-        "Fetch earlier statement; start with most recent."
+        """Fetch earlier statement; start with most recent."""
         self.fetch(reverse=True)
         return "break"
 
     def fetch(self, reverse):
-        '''Fetch statememt and replace current line in text widget.
+        """Fetch statememt and replace current line in text widget.
 
         Set prefix and pointer as needed for successive fetches.
         Reset them to None, None when returning to the start line.
         Sound bell when return to start line or cannot leave a line
         because cyclic is False.
-        '''
-        nhist = len(self.history)
+        """
+        self.check_changed()
+
+        nhist = len(self.super_history)
         pointer = self.pointer
         prefix = self.prefix
-        if pointer is not None and prefix is not None:
-            if self.text.compare("insert", "!=", "end-1c") or \
-                    self.text.get("iomark", "end-1c") != self.history[pointer]:
-                pointer = prefix = None
-                self.text.mark_set("insert", "end-1c")  # != after cursor move
+        suggested = self.suggested
+
+        # If pointer or prefix are not set, maybe because of reset above, need to get them.
         if pointer is None or prefix is None:
             prefix = self.text.get("iomark", "end-1c")
             if reverse:
@@ -70,48 +111,71 @@ class History:
                 else:  # abort history_next
                     self.text.bell()
                     return
+
+        suggested_items = zip(*suggested)[0] if suggested else []
+
         nprefix = len(prefix)
         while 1:
             pointer += -1 if reverse else 1
+
+            # Is it the end of the line?
             if pointer < 0 or pointer >= nhist:
                 self.text.bell()
+
+                # Yes.
                 if not self.cyclic and pointer < 0:  # abort history_prev
                     return
+                # No.
                 else:
                     if self.text.get("iomark", "end-1c") != prefix:
                         self.text.delete("iomark", "end-1c")
                         self.text.insert("iomark", prefix)
                     pointer = prefix = None
                 break
-            item = self.history[pointer]
-            if item[:nprefix] == prefix and len(item) > nprefix:
+
+            # Check the line for a match.
+            real_pointer, item = self.super_history[pointer]
+
+            if item not in suggested_items and item[:nprefix] == prefix and len(item) > nprefix:
                 self.text.delete("iomark", "end-1c")
                 self.text.insert("iomark", item)
-                self.histwin.goto(pointer)
+                self.histwin.goto(real_pointer)
+                suggested.append((item, pointer))
                 break
+
         self.text.see("insert")
         self.text.tag_remove("sel", "1.0", "end")
         self.pointer = pointer
         self.prefix = prefix
 
     def store(self, source):
-        "Store Shell input statement into history list."
+        """Store Shell input statement into history list."""
         source = source.strip()
-        if len(source) > 0:
-            # avoid duplicates
-            try:
-                idx = self.history.index(source)
-                del self.history[idx]
-                self.histwin.remove(idx)
-                self.ph.remove(source)
-            except ValueError:
-                pass
-            self.history.append(source)
-            self.ph.append(source)
+        history = self.history
 
+        if len(source) > 0:
+            # don't avoid duplicates
+            # try:
+            #     idx = self.history.index(source)
+            #     del self.history[idx]
+            #     self.histwin.remove(idx)
+            #     self.ph.remove(source)
+            # except ValueError:
+            #     pass
+            history.append(source)
+            self.ph.append(source)
             self.histwin.store(source)
+
+            self.super_history = list(enumerate(history))
+            for i in range(len(history) - 1, 0, -1):
+                if history[i - 1] == source:
+                    self.super_history.append((i, history[i]))
+                    break
+
         self.pointer = None
         self.prefix = None
+        self.suggested[:] = []
+
 
 if __name__ == "__main__":
     from unittest import main
