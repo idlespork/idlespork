c80dee371c7c37e381c1c47c55fbe1d962af3444
Dror Speiser <dror@bugatone.com>
2017-08-16 12:25:54 +0300

    added time difference directives in CustomizePrompt, and execution time in ResourcesStatus

--- idlesporklib/CustomizePrompt.py
+++ idlesporklib/CustomizePrompt.py
@@ -8,19 +8,47 @@ from idlesporklib.configHandler import idleConf
 
 class CustomizePrompt(object):
     """
-    Extension to customize prompt line. Whatever you enter is given to `time.strftime`.
+    Extension to customize prompt line. Whatever you enter is given to `time.strftime`,
+    with the following three new directives:
+        %dM - minutes since last execution
+
+        %dS - seconds since last execution
+
+        %df -
     """
 
     _PROMPT_FORMAT = idleConf.GetOption("extensions", "CustomizePrompt", "prompt-format", type="str", default='>>> ',
                                         member_name="_PROMPT_FORMAT")
 
+    last_prompt = None
+
     def __init__(self, editwin):
         def showprompt(self_):
+            if CustomizePrompt.last_prompt:
+                time_diff = time.time() - CustomizePrompt.last_prompt
+                mins, secs = divmod(time_diff, 60)
+                mins, wsecs = int(mins), int(secs)
+                ms = int((secs - wsecs) * 100)
+            else:
+                mins, wsecs, ms = 0, 0, 0
+
+            s = CustomizePrompt._PROMPT_FORMAT.strip()
+            s = s.replace('%dM', '%02d' % mins)
+            s = s.replace('%dS', '%02d' % wsecs)
+            s = s.replace('%df', '%02d' % ms)
+
             self_.resetoutput()
-            s = time.strftime(CustomizePrompt._PROMPT_FORMAT.strip() + ' ')
+            s = time.strftime(s + ' ')
             self_.console.write(s)
             self_.text.mark_set("insert", "end-1c")
             self_.set_line_and_column()
             self_.io.reset_undo()
 
+        old_runcmd_from_source = editwin.interp.runcmd_from_source
+
+        def runcmd_from_source(self_, line):
+            CustomizePrompt.last_prompt = time.time()
+            return old_runcmd_from_source(line)
+
         editwin.showprompt = MethodType(showprompt, editwin)
+        editwin.interp.runcmd_from_source = MethodType(runcmd_from_source, editwin.interp)
--- idlesporklib/ResourcesStatus.py
+++ idlesporklib/ResourcesStatus.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python
 from __future__ import print_function
 
+import time
+from types import MethodType
 from Tkinter import RIGHT
 from idlesporklib.EnablableExtension import EnablableExtension
 
@@ -13,17 +15,48 @@ except ImportError:
 class ResourcesStatus(EnablableExtension):
     """
     Extension to show cpu and memory usage in the status bar.
+    Also shows time since last execution.
 
     * Must have the package psutil installed.
     """
+
+    last_exec = None
+
     def __init__(self, editwin):
         if psutil is not None:
             self.editwin = editwin
             self.text = self.editwin.text
             try:
                 interp = self.editwin.interp
+
+                old_runcmd_from_source = editwin.interp.runcmd_from_source
+
+                def runcmd_from_source(self_, line):
+                    ResourcesStatus.last_exec = time.time()
+                    ret = old_runcmd_from_source(line)
+                    return ret
+
+                editwin.interp.runcmd_from_source = MethodType(runcmd_from_source, editwin.interp)
+
+                old_endexecuting = editwin.endexecuting
+
+                def endexecuting(self_):
+                    ResourcesStatus.last_exec = None
+                    return old_endexecuting()
+
+                editwin.endexecuting = MethodType(endexecuting, editwin)
+
+                old_restart_shell = editwin.restart_shell
+
+                def restart_shell(self_, event=None):
+                    ResourcesStatus.last_exec = None
+                    return old_restart_shell(event)
+
+                editwin.restart_shell = MethodType(restart_shell, editwin)
+
                 self.editwin.status_bar.set_label('cpu', 'Cpu: ?', side=RIGHT)
                 self.editwin.status_bar.set_label('mem', 'Mem: ?', side=RIGHT)
+                self.editwin.status_bar.set_label('time', '', side=RIGHT)
                 self.text.after_idle(self.set_cpu_and_mem)
             except AttributeError:
                 pass
@@ -38,7 +71,16 @@ class ResourcesStatus(EnablableExtension):
             mem = process.memory_percent()
             self.editwin.status_bar.set_label('cpu', 'Cpu: %.1f' % cpu)
             self.editwin.status_bar.set_label('mem', 'Mem: %.1f' % mem)
+
+            if ResourcesStatus.last_exec:
+                time_diff = int(time.time() - ResourcesStatus.last_exec)
+                mins, secs = divmod(time_diff, 60)
+                self.editwin.status_bar.set_label('time', '%02d:%02d' % (mins, secs))
+            else:
+                self.editwin.status_bar.set_label('time', '')
+
             self.text.after(1000, self.set_cpu_and_mem)
         else:
             self.editwin.status_bar.set_label('cpu', '')
             self.editwin.status_bar.set_label('mem', '')
+            self.editwin.status_bar.set_label('time', '')
